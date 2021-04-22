612b98c3423d39b8ef2c8a0641bb75d059c83395
AllenWarner7 <78972908+AllenWarner7@users.noreply.github.com>
2021-04-22 13:27:38 -0400

    bk

---
+++ Basketball Game copy/Assets/deployballs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deployballs : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public float respawnTime = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(newBall());
+    }
+    private void spawnBall()
+    {
+        GameObject ball = Instantiate(ballPrefab) as GameObject;
+    }
+    IEnumerator newBall()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnBall();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
